3c83a4fac1b9af33c536a675e876550d912f86d0
Anastasia <z.anastasia11@gmail.com>
2023-03-08 21:24:06 -0800

    clean up comments

--- index.js
+++ index.js
@@ -29,14 +29,7 @@ const tasks = [task1, task2];
 
 task1.logState();  // Clean Cat Litter has not been completed
 task1.markCompleted();
-task1.logState logTaskState(); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has been completed
 
 console.log(tasks)
-// DRIVER CODE BELOW
 
-// newTask("Clean Cat Litter"); // task 0
-// newTask("Do Laundry"); // task 1
-
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
